3f9686dfe831b7b428bcb8896749b1f43381e43f
Thiago Frensch <thiago.frensch@gmail.com>
2017-02-22 11:57:07 -0300

    first commit, app working, but need some adjusts

---
+++ detection.py
@@ -0,0 +1,219 @@
+# coding=utf-8
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+def drawMatches(img1, kp1, img2, kp2, matches):
+    """
+    My own implementation of cv2.drawMatches as OpenCV 2.4.9
+    does not have this function available but it's supported in
+    OpenCV 3.0.0
+
+    This function takes in two images with their associated 
+    keypoints, as well as a list of DMatch data structure (matches) 
+    that contains which keypoints matched in which images.
+
+    An image will be produced where a montage is shown with
+    the first image followed by the second image beside it.
+
+    Keypoints are delineated with circles, while lines are connected
+    between matching keypoints.
+
+    img1,img2 - Grayscale images
+    kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint 
+              detection algorithms
+    matches - A list of matches of corresponding keypoints through any
+              OpenCV keypoint matching algorithm
+    """
+
+    # Create a new output image that concatenates the two images together
+    # (a.k.a) a montage
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
+
+    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+
+    # Place the first image to the left
+    out[:rows1,:cols1] = np.dstack([img1, img1, img1])
+
+    # Place the next image to the right of it
+    out[:rows2,cols1:] = np.dstack([img2, img2, img2])
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1,y1) = kp1[img1_idx].pt
+        (x2,y2) = kp2[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)   
+        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+
+
+    # Show the image
+    cv2.imshow('Matched Features', out)
+    cv2.waitKey(0)
+    cv2.destroyWindow('Matched Features')
+
+    # Also return the image if you'd like a copy
+    return out
+
+def calcMatchesPosition(img1, kp1, img2, kp2, matches):
+    error_total_x = 0
+    error_total_y = 0
+
+    # para calcular a proporção da imagem
+    pos1 = 0
+    img1_idx = matches[pos1].queryIdx
+    img2_idx = matches[pos1].trainIdx
+
+    (pt1x1, pt1y1) = kp1[img1_idx].pt
+    (pt1x2, pt1y2) = kp2[img2_idx].pt
+
+    ratio_x = 0
+    ratio_y = 0
+
+    count_x = 0
+    count_y = 0
+    min_ratio = 0.1
+    max_ratio = 10.0
+    for pos2 in range(pos1 + 1, len(matches)):
+        img1_idx2 = matches[pos2].queryIdx
+        img2_idx2 = matches[pos2].trainIdx
+
+        (pt2x1, pt2y1) = kp1[img1_idx2].pt
+        (pt2x2, pt2y2) = kp2[img2_idx2].pt
+
+        if (pt1x2 - pt2x2) != 0:
+            ratio = (pt1x1 - pt2x1) / (pt1x2 - pt2x2)
+            if ratio > min_ratio and ratio < max_ratio:
+                ratio_x += ratio
+                count_x += 1
+        if (pt1y2 - pt2y2) != 0:
+            ratio = (pt1y1 - pt2y1) / (pt1y2 - pt2y2)
+            if ratio > min_ratio and ratio < max_ratio:
+                ratio_y += ratio
+                count_y += 1
+
+    if count_x > 0:
+        ratio_x /= count_x
+    if count_y > 0:
+        ratio_y /= count_y
+    print 'ratiox: ' + str(ratio_x)
+    print 'ratioy: ' + str(ratio_y)
+
+    # para calcular o erro de posicionamento de cada ponto
+    for pos1 in range(len(matches)-1):
+
+        # Get the matching keypoints for each of the images
+        img1_idx = matches[pos1].queryIdx
+        img2_idx = matches[pos1].trainIdx
+
+        (pt1x1,pt1y1) = kp1[img1_idx].pt
+        (pt1x2,pt1y2) = kp2[img2_idx].pt
+
+        error_x = 0
+        error_y = 0
+        for pos2 in range(pos1+1, len(matches)):
+            img1_idx2 = matches[pos2].queryIdx
+            img2_idx2 = matches[pos2].trainIdx
+
+            (pt2x1, pt2y1) = kp1[img1_idx2].pt
+            (pt2x2, pt2y2) = kp2[img2_idx2].pt
+
+            error_x += np.fabs((pt1x1 - pt2x1) - ((pt1x2 - pt2x2) * ratio_x))
+            error_y += np.fabs((pt1y1 - pt2y1) - ((pt1y2 - pt2y2) * ratio_y))
+
+        error_x /= (len(matches)- (pos1+1))
+        error_y /= (len(matches)- (pos1+1))
+
+        error_total_x += error_x
+        error_total_y += error_y
+    return (error_total_x/ratio_x + error_total_y/ratio_y) / 2
+
+
+db_imgs = []
+db_files = []#['db/monalisa.jpg', 'db/starwars1.jpg', 'db/starwars2.jpg']
+db_kp = []
+db_des = []
+# Create ORB detector with 1000 keypoints with a scaling pyramid factor of 1.2
+orb = cv2.ORB(1000, 1.2)
+# Load images from database
+import glob, os
+for file in glob.glob("./db/*.jpg"):
+    print(file)
+    db_files.append(file)
+
+for i_img in range(len(db_files)):
+    img = cv2.imread(db_files[i_img], 0)
+    cols,rows = img.shape
+    img = cv2.resize(img, (500*rows/cols,500))
+    #cv2.imshow('teste', img)
+    #cv2.waitKey(0)
+    db_imgs.append(img)
+    (kp, des) = orb.detectAndCompute(db_imgs[i_img], None)
+    db_kp.append(kp)
+    db_des.append(des)
+# Create matcher
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+def findMatch(img_filename):
+
+    error_imgs = []
+    img = cv2.imread(img_filename, 0)
+    cols,rows = img.shape
+    img = cv2.resize(img, (500*rows/cols,500))
+
+
+    # Detect keypoints of original image
+    (kp1,des1) = orb.detectAndCompute(img, None)
+
+    for i_img in range(len(db_imgs)):
+        # Do matching
+        matches = bf.match(des1,db_des[i_img])
+
+        # Sort the matches based on distance.  Least distance
+        # is better
+        matches = sorted(matches, key=lambda val: val.distance, reverse=False)
+        sum = 0
+        total = 20
+        for num in range(0,total):
+            print 'matches: ' + str(matches[num].distance)
+            sum += matches[num].distance
+        sum /= total
+        print 'media: ' + str(sum)
+        # Show only the top 10 matches - also save a copy for use later
+
+        error = calcMatchesPosition(img, kp1, db_imgs[i_img], db_kp[i_img], matches[0:total])
+
+        error_imgs.append(error)
+        #out = drawMatches(img1, kp1, img2, kp2, matches[0:total])
+
+    best_match = 0
+    smallest_error = error_imgs[0]
+    for num in range(1, len(db_imgs)):
+        if error_imgs[num] < smallest_error:
+            smallest_error = error_imgs[num]
+            best_match = num
+
+    print 'ERROR: ' + str(smallest_error)
+    if smallest_error > 1000:
+        return 'it didn\'t match any files!'
+    return db_files[best_match]
